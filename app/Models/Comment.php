b367b9d73609c3a0d2f97767d891c8a4c1e9c06f
putheakhem <puthea.khem@gmail.com>
2022-07-29 15:28:36 +0700

    one to many relatioship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Comment extends Model
 {
     use HasFactory;
 
-    public function comments() {
-        return $this->hasMany(Comment::class);
+    public function post() {
+        return $this->belongsTo(Post::class);
     }
 }
