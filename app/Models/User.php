4b60041779b91cc1e3b5d9e2e01cc0b3870a2347
putheakhem <puthea.khem@gmail.com>
2022-08-09 20:41:08 +0700

    many to many

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
     public function account() {
         return $this->hasOne(Account::class);
     }
+
+   public function roles() {
+        return $this->belongsToMany(Role::class, 'role_user');
+   }
 }
